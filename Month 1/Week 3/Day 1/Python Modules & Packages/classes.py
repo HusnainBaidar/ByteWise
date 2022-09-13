fdb242bf0ff175e8aca5a8a0cae8667d849dcb32
Husnain Baidar <75679355+HusnainBaidar@users.noreply.github.com>
2022-09-13 20:12:26 +0500

    Add files via upload

---
+++ Month 1/Week 3/Day 1/Python Modules & Packages/classes.py
@@ -0,0 +1,11 @@
+from space.planet import Planet
+from space.calc import planet_mass, planet_vol
+
+
+naboo = Planet('Naboo',30000,8,'Milky Way')
+
+naboo_mass = planet_mass (naboo.gravity,naboo.radius)
+
+naboo_vol = planet_vol (naboo.radius)
+
+print (f'{naboo.name} has a mass of {naboo_mass} and a volume of {naboo_vol}')
